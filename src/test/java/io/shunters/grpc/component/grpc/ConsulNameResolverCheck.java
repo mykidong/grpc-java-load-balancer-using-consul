e81268772ef064bf87f03c51a11bc2e3a61ffa17
Kidong Lee <mykidong@gmail.com>
2018-01-13 11:35:49 +0900

    log4j loading added...

--- src/test/java/io/shunters/grpc/component/grpc/ConsulNameResolverCheck.java
+++ src/test/java/io/shunters/grpc/component/grpc/ConsulNameResolverCheck.java
@@ -1,12 +1,20 @@
 package io.shunters.grpc.component.grpc;
 
+import org.apache.log4j.xml.DOMConfigurator;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ConsulNameResolverCheck {
 
+    @Before
+    public void init() throws Exception {
+        // load log4j.
+        java.net.URL url = new ConsulNameResolverCheck().getClass().getResource("/log4j-test.xml");
+        DOMConfigurator.configure(url);
+    }
+
     @Test
-    public void checkConsulNameResolver()
-    {
+    public void checkConsulNameResolver() {
         HelloWorldClientWithNameResolver client = new HelloWorldClientWithNameResolver("emb-collection-handler", "localhost", 8500);
     }
 }
